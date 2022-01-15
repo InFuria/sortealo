7417558e675d0eb86979750c1775875eb9b12e57
vagrant <vagrant@homestead>
2022-01-15 08:48:02 +0000

    cors up

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,6 +40,7 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RealRashid\SweetAlert\ToSweetAlert::class,
+            \Fruitcake\Cors\HandleCors::class
         ],
 
         'api' => [
