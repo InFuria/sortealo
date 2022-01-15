3f08ac1b902eae01387dd786dbdd34ea612308f1
vagrant <vagrant@homestead>
2022-01-15 08:50:09 +0000

    cors up 2

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -22,7 +22,7 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \Illuminate\Session\Middleware\StartSession::class,
-        \Fruitcake\Cors\HandleCors::class
+        \Fruitcake\Cors\HandleCors::class,
     ];
 
     /**
@@ -40,7 +40,6 @@ class Kernel extends HttpKernel
             \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \RealRashid\SweetAlert\ToSweetAlert::class,
-            \Fruitcake\Cors\HandleCors::class
         ],
 
         'api' => [
